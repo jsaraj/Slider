c1cbe20a488c2a47e94b641809a394b5b506b673
jaber saraj <jaber2x@gmail.com>
2024-09-09 18:24:39 +0330

    Added Pre and Next Action and setIntrval To Project and Complete it

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -6,28 +6,28 @@ const data =[
         id:1,
         name:"Jaber",
         title:"IT Man",
-        image:"",
+        image:"https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates fugiat aperiam ea illum, veniam consequuntur quisquam assumenda totam porro animi, ducimus esse a sit voluptatem repudiandae accusamus recusandae suscipit!"
     },
     {
         id:2,
-        name:"JJavid",
+        name:"Javid",
         title:"Programmer",
-        image:"",
+        image:"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates fugiat aperiam ea illum, veniam consequuntur quisquam assumenda totam porro animi, ducimus esse a sit voluptatem repudiandae accusamus recusandae suscipit!"
     },
     {
         id:1,
         name:"Roya",
         title:"Graphic",
-        image:"",
+        image:"https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates fugiat aperiam ea illum, veniam consequuntur quisquam assumenda totam porro animi, ducimus esse a sit voluptatem repudiandae accusamus recusandae suscipit!"
     },
     {
         id:1,
         name:"Paria",
         title:"Architect",
-        image:"",
+        image:"https://images.pexels.com/photos/1462637/pexels-photo-1462637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptates fugiat aperiam ea illum, veniam consequuntur quisquam assumenda totam porro animi, ducimus esse a sit voluptatem repudiandae accusamus recusandae suscipit!"
     },
 
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,16 +1,79 @@
 
 
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import data from './Data'
+import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 
 
 function Slider() {
-  return (
-    <>
-    
-    </>
-  )
+
+    const [people, setPeople] = useState(data);
+    const [index, setIndex] = useState(0);
+
+
+    useEffect(() => {
+        if (index < 0) {
+            setIndex(people.length - 1)
+        }
+        if (index > (people.length - 1)) {
+            setIndex(0);
+        }
+    }, [index, people])
+
+
+    useEffect(()=>{
+        let sliderTime=setInterval(() => {
+            setIndex(index+1)
+        }, 1000);
+        return()=> clearInterval(sliderTime)
+    },[index])
+
+
+    return (
+        <>
+            <div className='justify-center flex text-2xl font-semibold pt-8 pb-12 '>
+                <h2 className='w-2/12 border-b border-orange-400 flex justify-center pb-2'>Customer Comments</h2>
+            </div>
+
+            <div className='relative'>
+
+                {
+                    people.map((person, personIndex) => {
+                        const { id, name, title, image, desc } = person
+                        let position = "translate-x-[100%]";
+                        if (personIndex === index) {
+                            position = "translate-x-[0%]"
+                        }
+                        if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+                            position = "-translate-x-[100%]"
+                        }
+                        return (
+                            <div key={id} className={` ${position} 'transition-transform duration-[400ms] ease-in-out flex justify-center relative '`}>
+                                <div className='absolute'>
+                                    <img src={image} alt={name} className='h-64 w-64 rounded-full'></img>
+                                </div>
+                                <div className=' text-2xl absolute top-[280px]'>
+                                    <h3 className='border-b border-orange-400' >{name}</h3>
+                                </div>
+                                <div className=' text-lg absolute top-[330px]'>
+                                    <h4>{title}</h4>
+                                </div>
+                                <div className=' absolute top-[390px]'>
+                                    <p className='text-justify px-96 leading-7'>{desc}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                <div className='absolute top-[300px] right-20 text-2xl'>
+                    <BsArrowRightCircle onClick={() => setIndex(index + 1)} />
+                </div>
+                <div className='absolute top-[300px] left-20 text-2xl'>
+                    <BsArrowLeftCircle onClick={() => setIndex(index - 1)} />
+                </div>
+            </div >
+        </>
+    )
 }
 
 export default Slider
